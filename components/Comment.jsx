6082287b6bb7a12a31e048ddc464db2bc97dfb2e
Yeargao <ys0958326906@gmail.com>
2025-07-21 12:10:10 +0800

    update Enemy.jsx update Comment.jsx

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 
 function Comment() {
-  const [Com, setCom] = useState([])
+const [Com, setCom] = useState([
+    {content:'1',id: Math.random()},
+    {content:'2',id: Math.random()}
+  ])
+
+  const addCom=(content)=>{
+    content.preventDefault()
+    setCom(...Com),{content,id: Math.random()}
+  }
 
   return (
-    <form>
+    <form className="rightDown" onSubmit={addCom}>
       <input type="text" placeholder="輸入" value={Com} onChange={(e) => { setCom(e.target.value) }} style={{ position: "absolute", left: '780px', top: '540px' }} />
       <button type="submit" style={{ position: "absolute", left: '1020px', top: '540px' }}>加入</button>
       <ul style={{ listStyleType: "none" }}>
-        <li style={{ position: "absolute", left: '780px', top: '600px' }}>
-          {Com}
-        </li>
+        <li style={{ position: "absolute", left: '780px', top: '600px' }}>{Com[0]}</li>
       </ul>
     </form>
   )
 }
+ 
 
 export default Comment
