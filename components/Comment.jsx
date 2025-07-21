a8127abe29c4371f73f0f93c97ca33883e7a6323
Yeargao <ys0958326906@gmail.com>
2025-07-21 12:15:32 +0800

    update Comment.jsx

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 function Comment() {
+const [content, setContent] = useState('')
 const [Com, setCom] = useState([
     {content:'1',id: Math.random()},
     {content:'2',id: Math.random()}
@@ -9,11 +10,12 @@ const [Com, setCom] = useState([
   const addCom=(content)=>{
     content.preventDefault()
     setCom(...Com),{content,id: Math.random()}
+    setContent('')
   }
 
   return (
     <form className="rightDown" onSubmit={addCom}>
-      <input type="text" placeholder="輸入" value={Com} onChange={(e) => { setCom(e.target.value) }} style={{ position: "absolute", left: '780px', top: '540px' }} />
+      <input type="text" placeholder="輸入" value={content} onChange={(e) => { setCom(e.target.value) }} style={{ position: "absolute", left: '780px', top: '540px' }} />
       <button type="submit" style={{ position: "absolute", left: '1020px', top: '540px' }}>加入</button>
       <ul style={{ listStyleType: "none" }}>
         <li style={{ position: "absolute", left: '780px', top: '600px' }}>{Com[0]}</li>
@@ -24,3 +26,5 @@ const [Com, setCom] = useState([
  
 
 export default Comment
+
+//update Comment
